2902c6c7ca2ad3e0db0c941b91d45963f84d3df0
Gary Rennie <gazler@gmail.com>
2025-04-11 09:35:40 +0100

    refactor(examples/colors): extract color_block into a function

--- examples/colors.exs
+++ examples/colors.exs
@@ -4,75 +4,48 @@ defmodule Colors do
     |> draw()
   end
 
+  defp newline(str, i, i), do: str <> "\n"
+  defp newline(str, _i, _limit), do: str
+
+  defp color_block(foreground, background, padding \\ 3) do
+    Termite.Style.ansi256()
+    |> Termite.Style.background(background)
+    |> Termite.Style.foreground(foreground)
+    |> Termite.Style.render_to_string(
+      " #{String.pad_leading(to_string(background), padding, " ")}   "
+    )
+  end
+
   def draw(term) do
     str = Termite.Style.bold() |> Termite.Style.render_to_string("Basic ANSI Colors\n")
-
     term = Termite.Screen.write(term, str)
 
     term =
       Enum.reduce(0..15, term, fn i, acc ->
         foreground = if i < 5, do: 7, else: 0
-
-        str =
-          Termite.Style.background(i)
-          |> Termite.Style.foreground(foreground)
-          |> Termite.Style.render_to_string(" #{String.pad_leading(to_string(i), 2, " ")}   ")
-
-        str =
-          if i == 7 do
-            str <> "\n"
-          else
-            str
-          end
-
-        Termite.Screen.write(acc, str)
+        str = color_block(foreground, i, 2)
+        Termite.Screen.write(acc, newline(str, i, 7))
       end)
 
     str = Termite.Style.bold() |> Termite.Style.render_to_string("\n\nExtended ANSI Colors\n")
-
     term = Termite.Screen.write(term, str)
 
-    Enum.reduce(16..231, term, fn i, acc ->
-      foreground = if i < 28, do: 7, else: 0
-
-      str =
-        Termite.Style.ansi256()
-        |> Termite.Style.background(i)
-        |> Termite.Style.foreground(foreground)
-        |> Termite.Style.render_to_string(" #{String.pad_leading(to_string(i), 3, " ")}   ")
-
-      str =
-        if rem(i - 16, 6) == 5 do
-          str <> "\n"
-        else
-          str
-        end
-
-      Termite.Screen.write(acc, str)
-    end)
+    term =
+      Enum.reduce(16..231, term, fn i, acc ->
+        foreground = if i < 28, do: 7, else: 0
+        str = color_block(foreground, i)
+        Termite.Screen.write(acc, newline(str, rem(i - 16, 6), 5))
+      end)
 
     str = Termite.Style.bold() |> Termite.Style.render_to_string("\n\Extended ANSI Grayscale\n")
-
     term = Termite.Screen.write(term, str)
 
-    Enum.reduce(232..255, term, fn i, acc ->
-      foreground = if i < 244, do: 7, else: 0
-
-      str =
-        Termite.Style.ansi256()
-        |> Termite.Style.background(i)
-        |> Termite.Style.foreground(foreground)
-        |> Termite.Style.render_to_string(" #{String.pad_leading(to_string(i), 3, " ")}   ")
-
-      str =
-        if rem(i - 232, 6) == 5 do
-          str <> "\n"
-        else
-          str
-        end
-
-      Termite.Screen.write(acc, str)
-    end)
+    term =
+      Enum.reduce(232..255, term, fn i, acc ->
+        foreground = if i < 244, do: 7, else: 0
+        str = color_block(foreground, i)
+        Termite.Screen.write(acc, newline(str, rem(i - 232, 6), 5))
+      end)
 
     Termite.Screen.write(term, "\n")
   end
