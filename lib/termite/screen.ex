61237e13caac71f5275b0913f388092c86963165
Gary Rennie <gazler@gmail.com>
2025-04-30 13:56:02 +0100

    feat(Termite.Screen): add functions for escape sequences
    All of the escape sequences previously defined now include separate
    functions. This improves the discoverability of the sequences as well
    as providing documentation for them.

--- lib/termite/screen.ex
+++ lib/termite/screen.ex
@@ -12,15 +12,13 @@ defmodule Termite.Screen do
   defp seq(:cursor_previous_line, [n]), do: "#{n}F"
   defp seq(:cursor_move, [y, x]), do: "#{x};#{y}H"
 
-  defp seq(:screen_clear, []), do: "J"
+  defp seq(:screen_clear, []), do: "2J"
 
   defp seq(:screen_alt, []), do: "?1049h"
   defp seq(:screen_alt_exit, []), do: "?1049l"
 
   defp seq(:cursor_show, []), do: "?25h"
   defp seq(:cursor_hide, []), do: "?25l"
-
-  defp seq(:reset, []), do: "r"
 
   @doc """
   Return the escape code for the terminal.
@@ -51,6 +49,90 @@ defmodule Termite.Screen do
   """
   def run_escape_sequence(term, command, args \\ []) do
     write(term, escape_sequence(command, args))
+  end
+
+  @doc """
+  Move the cursor up by n lines.
+  """
+  def cursor_up(term, n \\ 1) do
+    run_escape_sequence(term, :cursor_up, [n])
+  end
+
+  @doc """
+  Move the cursor down by n lines.
+  """
+  def cursor_down(term, n \\ 1) do
+    run_escape_sequence(term, :cursor_down, [n])
+  end
+
+  @doc """
+  Move the cursor forward by n cells.
+  """
+  def cursor_forward(term, n \\ 1) do
+    run_escape_sequence(term, :cursor_forward, [n])
+  end
+
+  @doc """
+  Move the cursor back by n cells.
+  """
+  def cursor_back(term, n \\ 1) do
+    run_escape_sequence(term, :cursor_back, [n])
+  end
+
+  @doc """
+  Move the cursor down by n lines and place cursor at the beginning of the line.
+  """
+  def cursor_next_line(term, n \\ 1) do
+    run_escape_sequence(term, :cursor_next_line, [n])
+  end
+
+  @doc """
+  Move the cursor up by n lines and place cursor at the beginning of the line.
+  """
+  def cursor_previous_line(term, n \\ 1) do
+    run_escape_sequence(term, :cursor_previous_line, [n])
+  end
+
+  @doc """
+  Set the cursor position to x,y.
+  """
+  def cursor_position(term, x, y) do
+    run_escape_sequence(term, :cursor_move, [x, y])
+  end
+
+  @doc """
+  Show the cursor.
+  """
+  def show_cursor(term) do
+    run_escape_sequence(term, :cursor_show, [])
+  end
+
+  @doc """
+  Hide the cursor.
+  """
+  def hide_cursor(term) do
+    run_escape_sequence(term, :cursor_hide, [])
+  end
+
+  @doc """
+  Clears the screen.
+  """
+  def clear_screen(term) do
+    run_escape_sequence(term, :screen_clear, [])
+  end
+
+  @doc """
+  Switch to alt screen.
+  """
+  def alt_screen(term) do
+    run_escape_sequence(term, :screen_alt, [])
+  end
+
+  @doc """
+  Exit to alt screen.
+  """
+  def exit_alt_screen(term) do
+    run_escape_sequence(term, :screen_alt_exit, [])
   end
 
   defdelegate write(term, str), to: Termite.Terminal
